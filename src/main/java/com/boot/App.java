348da054cd75e68413434ef9b87a6736e134b840
BaneleMlamleli <mlamlelibanele@yahoo.com>
2018-06-20 13:03:42 +0200

    Deleted some packages and I also moved model, repository, service and controller packages in the the com.boot root package.
    I added some few comments to explain some annotations.
    I also fixed the issue where I could not display the app on the Tomcat server

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -2,9 +2,19 @@ package com.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot application
+ * @author Shaun
+ */
+//To enable Spring Boot we need the following annotation
+/*
+ * This annotation will search the entire project for Spring components
+ * and it will automatically configure accordingly
+ * */
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
+
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/boot/controller/HomeController.java
+++ src/main/java/com/boot/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    //This code will run spring boot
     @RequestMapping("/")
     public String home(){
         return "Hello there Banele";
